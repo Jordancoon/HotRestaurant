336dea9cccefdfed2b83f8561f36768f597423a2
jordan <jgcoon24@gmail.com>
2018-09-29 10:59:04 -0500

    add routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,3 @@
+// load data //
+var tableData = require("../data/tableData");
+var waitListData = require("")
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+var require = require("path");
+
+// routing // 
+module.exports = function(app) {
+
+    app.get("/tables", function(app) {
+        res.sendFile(path.join(__dirname, "../public/tables.html"));
+    });
+
+    app.get("/reserve", function(app) {
+        res.sendFile(path.join(__dirname, "../public/reserve.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+
+});
+};
+
