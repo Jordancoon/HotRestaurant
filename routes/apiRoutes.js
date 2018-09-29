35b8c49d8d0737409ca1d3530da4003434708d0f
jordan <jgcoon24@gmail.com>
2018-09-29 11:27:11 -0500

    api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,34 @@
 // load data //
 var tableData = require("../data/tableData");
-var waitListData = require("")
+var waitListData = require("../data/waitinglist")
 ///
+
+module.exports = function(app) {
+
+    app.get("/api/tables", function(req, res) {
+        res.json(tableData);
+    });
+
+    app.get("/api/waitlist", function(req, res) {
+        res.json(waiListData);
+    });
+
+    app.post("/api/tables", function(req, res) {
+        if (tableData.length < 5) {
+            tableData.push(req.body);
+            res.json(true);
+        }
+        else {
+            waitListData.push(req.body);
+            res.json(false);
+        }
+    });
+
+    /// clear out table //
+    app.post("/api/clear", function() {
+        tableData = [];
+        waitListData = [];
+
+        console.log(tableData);
+    });
+};
