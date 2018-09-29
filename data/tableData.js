902dadbd6f1fcf94c7c3de53263359d4a170b1d5
jordan <jgcoon24@gmail.com>
2018-09-29 12:39:24 -0500

    add data?

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+var tableArray = [
+    {
+        customerName: "george",
+        customerEmail: "george@example.com",
+        customerID: "george1",
+        phoneNumber: " 000-000-0000"
+    }
+];
+
+module.exports = tableArray;
